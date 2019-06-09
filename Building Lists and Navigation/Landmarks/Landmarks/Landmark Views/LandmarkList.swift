a9ba98903d4558d71f630f1b56d4322f5ac8dba6
aryzae <a.aryzae@gmail.com>
2019-06-10 01:26:54 +0900

    modify Section 4

---
+++ Building Lists and Navigation/Landmarks/Landmarks/Landmark Views/LandmarkList.swift
@@ -0,0 +1,26 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by aryzae on 2019/06/10.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList : View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarkData[0])
+            LandmarkRow(landmark: landmarkData[1])
+        }
+    }
+}
+
+#if DEBUG
+struct LandmarkList_Previews : PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
+#endif
