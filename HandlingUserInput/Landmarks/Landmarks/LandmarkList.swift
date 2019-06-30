15c8cef16511a8570df7556fa5919ebdafedf573
aryzae <a.aryzae@gmail.com>
2019-06-30 17:31:37 +0900

    modify Section 3

--- HandlingUserInput/Landmarks/Landmarks/LandmarkList.swift
+++ HandlingUserInput/Landmarks/Landmarks/LandmarkList.swift
@@ -8,14 +8,19 @@ A view showing a list of landmarks.
 import SwiftUI
 
 struct LandmarkList: View {
-    @State var showFavoritesOnly = false
+    @State var showFavoritesOnly = true
 
     var body: some View {
         NavigationView {
-            List(landmarkData) { landmark in
-                if !self.showFavoritesOnly || landmark.isFavorite {
-                    NavigationButton(destination: LandmarkDetail(landmark: landmark)) {
-                        LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                ForEach(landmarkData) { landmark in
+                    if !self.showFavoritesOnly || landmark.isFavorite {
+                        NavigationButton(destination: LandmarkDetail(landmark: landmark)) {
+                            LandmarkRow(landmark: landmark)
+                        }
                     }
                 }
             }
