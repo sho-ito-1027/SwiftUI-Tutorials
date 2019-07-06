f52ad0b2287003c2233b870d5d3e1d644077e6c2
aryzae <a.aryzae@gmail.com>
2019-07-07 01:07:00 +0900

    modify Section 1

---
+++ DrawingPathsAndShapes/Landmarks/Landmarks/Badge.swift
@@ -0,0 +1,23 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by aryzae on 2019/07/06.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge : View {
+    var body: some View {
+        Text("Badge")
+    }
+}
+
+#if DEBUG
+struct Badge_Previews : PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
+#endif
