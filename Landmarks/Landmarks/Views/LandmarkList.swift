fc552756b33bfb70e8cac84d69b9485ecc4ad3ad
shoito <a.aryzae@gmail.com>
2021-02-06 21:13:24 +0900

    add Section4 in Chap2

---
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by aryzae on 2021/02/06.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
