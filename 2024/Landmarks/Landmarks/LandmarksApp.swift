fed5750f198ce353296d9ca90b186f74097fb1e9
shoito <sho.ito@p.radiko.co.jp>
2024-01-21 15:14:46 +0900

    add Landmark Project

---
+++ 2024/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by NP_202301_04 on 2024/01/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
